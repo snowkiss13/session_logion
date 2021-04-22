c1850e3a345235be65c21aa00966c31a6eedf915
snowkiss13 <snowkiss13@gmail.com>
2021-04-23 08:23:04 +0900

    489 Createアクションを実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,12 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  def create
+    @user = USER.new(user_params)
+  end
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
 end
