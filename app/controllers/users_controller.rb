e9c9ad3cf069cc6567f024cb346989d7b5d07c38
snowkiss13 <snowkiss13@gmail.com>
2021-04-24 17:39:19 +0900

    ログインシステム７ エラーメッセージの処理を行う。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,11 @@ class UsersController < ApplicationController
     @user = User.new
   end
   def create
-    @user = USER.new(user_params)
+    @user = User.new(user_params)
+    if @user.save
+    else
+      render :new
+    end
   end
   private
   def user_params
