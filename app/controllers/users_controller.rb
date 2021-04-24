b7e071a4f98a575a688aa814852f9e032c4a7114
snowkiss13 <snowkiss13@gmail.com>
2021-04-24 18:45:23 +0900

    ユーザー登録後にshowアクションにリダイレクトさせる処理を追記

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      redirect_to user_path(@user.id)
     else
       render :new
     end
